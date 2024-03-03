1de19ea0d393a3e345c2d7c584212c851bb440f2
olufsenglo <olsheaolufsen@gmail.com>
2024-03-03 13:46:29 +0100

    Add logo and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
+import SquarePlusIcon from "./icons/square-plus.svg?component";
+import LayoutIcon from "./icons/layout.svg?component";
+import Palette from "./icons/palette.svg?component";
+import FileIcon from "./icons/file.svg?component";
+import ImageIcon from "./icons/image.svg?component";
 
 function App() {
   return (
     <div className="flex bg-gray-100 font-sans text-gray-900">
       <aside className="flex h-screen w-18 flex-col items-center border-r border-gray-200 bg-white">
         <div className="flex h-18 w-full items-center justify-center border-b border-gray-200">
+          <img className="h-11 w-11" src="/img/logo.png" alt="" />
         </div>
         <nav className="flex flex-1 flex-col gap-y-4 pt-10">
+          <a
+            href="#"
+            className="group relative rounded-xl bg-gray-100 p-2 text-blue-600 hover:bg-gray-50"
+          >
+            <SquarePlusIcon className="h-6 w-6 stroke-current" />
+          </a>
+          <a
+            href="#"
+            className="group relative rounded-xl p-2 text-gray-400 hover:bg-gray-100"
+          >
+            <LayoutIcon className="h-6 w-6 stroke-current" />
+          </a>
+          <a
+            href="#"
+            className="group relative rounded-xl p-2 text-gray-400 hover:bg-gray-100"
+          >
+            <Palette className="h-6 w-6 stroke-current" />
+          </a>
+          <a
+            href="#"
+            className="group relative rounded-xl p-2 text-gray-400 hover:bg-gray-100"
+          >
+            <FileIcon className="h-6 w-6 stroke-current" />
+          </a>
+          <a
+            href="#"
+            className="group relative rounded-xl p-2 text-gray-400 hover:bg-gray-100"
+          >
+            <ImageIcon className="h-6 w-6 fill-current" />
+          </a>
         </nav>
       </aside>
       <div className="flex h-screen flex-1 flex-col">
