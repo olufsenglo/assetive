dcbe7a3c166b0e5aab15add69ea78a5808661592
olufsenglo <olsheaolufsen@gmail.com>
2024-03-03 14:06:50 +0100

    Add microinteraction to title

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import ImageIcon from "./icons/image.svg?component";
 import AlertCircleIcon from "./icons/alert-circle.svg?component";
 import SettingsIcon from "./icons/settings.svg?component";
 import BarsIcon from "./icons/bars.svg?component";
+import PencilIcon from "./icons/pencil.svg?component";
 
 function App() {
   return (
@@ -137,6 +138,24 @@ function App() {
             <section className="py-16">
               <div className="px-12">
                 <div className="group relative">
+                  <div className="pointer-events-none absolute inset-0 hidden border-2 border-blue-600 group-focus-within:block">
+                    <div className="absolute -translate-y-full pl-2">
+                      <div className="flex items-center gap-x-2 rounded-t-lg bg-blue-600 px-3 py-1 text-white">
+                        <span className="text-sm">H1 - hero title</span>
+                        <PencilIcon className="h-4 w-4 fill-current" />
+                      </div>
+                    </div>
+                    <div className="absolute left-0 top-0 h-2 w-2 -translate-x-full -translate-y-full border-2 border-blue-600"></div>
+                    <div className="absolute right-0 top-0 h-2 w-2 translate-x-full -translate-y-full border-2 border-blue-600"></div>
+                    <div className="absolute right-0 bottom-0 h-2 w-2 translate-x-full translate-y-full border-2 border-blue-600"></div>
+                    <div className="absolute left-0 bottom-0 h-2 w-2 -translate-x-full translate-y-full border-2 border-blue-600"></div>
+                    <div className="absolute inset-x-0 top-0 flex -translate-y-1/2 items-center justify-center">
+                      <div className="h-2 w-2 border-2 border-blue-600 bg-white"></div>
+                    </div>
+                    <div className="absolute inset-x-0 bottom-0 flex translate-y-1/2 items-center justify-center">
+                      <div className="h-2 w-2 border-2 border-blue-600 bg-white"></div>
+                    </div>
+                  </div>
                   <h1
                     className="max-w-3xl text-5xl font-bold leading-[1.4] focus:outline-none"
                     contentEditable
